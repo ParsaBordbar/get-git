aa5159e1a3bab0646d0feb7724853d167d63647f
ParsaBordbar <parsab71@gmail.com>
2025-03-21 03:27:24 +0330

    feat: MarkDownText component

---
+++ src/components/MarkDown/index.tsx
@@ -0,0 +1,7 @@
+import ReactMarkdown from 'react-markdown';
+
+const MarkDownText = ({ markdown } : {markdown: string}) => {
+  return <ReactMarkdown>{markdown}</ReactMarkdown>;
+};
+
+export default MarkDownText;
