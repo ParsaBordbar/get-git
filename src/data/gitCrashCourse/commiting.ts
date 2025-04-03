d7ecdaac7643e19151b8c6a195e0638dde8f0ce8
ParsaBordbar <parsab71@gmail.com>
2025-04-03 12:59:30 +0330

    feat(gitCrashCourse): data for basics and main menu

---
+++ src/data/gitCrashCourse/commiting.ts
@@ -0,0 +1,46 @@
+export const committing = [
+    {
+        title: 'Conventional Commits',  
+        kind:  'default',
+        desc: 'By Parsa Bordbar',
+        tags: [
+          {
+            text: 'git',
+            url: 'smt'
+          },
+          {
+            text: 'github',
+            url: 'smt'
+          },
+          {
+            text: 'version_control',
+            url: 'smt'
+          }
+        ], 
+        icon:  '/src/assets/git.svg', 
+        imgUrl:  '',
+        style: 'bg-[#7B9D7C]',
+      },
+      {
+        title: 'Structure',  
+        kind:  'default',
+        desc: 'By Parsa Bordbar',
+        tags: [
+          {
+            text: 'git',
+            url: 'smt'
+          },
+          {
+            text: 'github',
+            url: 'smt'
+          },
+          {
+            text: 'version_control',
+            url: 'smt'
+          }
+        ], 
+        icon:  '/src/assets/git.svg', 
+        imgUrl:  '',
+        style: 'bg-[#7B9D7C]',
+      },
+]
