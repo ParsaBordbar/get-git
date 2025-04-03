87ad406fd82552229c4f912692f42111cd7894f8
ParsaBordbar <parsab71@gmail.com>
2025-04-03 13:00:12 +0330

    feat(types): handle new types

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,12 +3,22 @@ export interface tagType {
     url: string,
 }
 
+export interface copyType {
+    label?: string,
+    text: string,
+}
 export interface cardType {
     title: string, 
     desc?: string, 
     kind: string, 
-    tags: tagType[], 
+    tags?: tagType[], 
     icon: string, 
     imgUrl?: string,
+    code?: copyType[],
     style: string,
+}
+
+export interface ReactionButtonType {
+    style?: string, 
+    text: string
 }
