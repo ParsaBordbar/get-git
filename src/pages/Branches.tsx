846bb8aab4fdf87d474594de47187eee88f8c433
ParsaBordbar <parsab71@gmail.com>
2025-04-25 12:04:45 +0330

    feat(pages): Branches page for github crash coures

--- src/pages/Branches.tsx
+++ src/pages/Branches.tsx
@@ -6,7 +6,7 @@ function Branches() {
     <section className="col-span-full flex justify-center items-center w-full lg:contain-none 
     md:contain-content contain-none">
         <FlashCardSeries data={branches} 
-                navigation={{prev:"http://localhost:5173/git/commit", next:"http://localhost:5173/git/branches"}}
+                navigation={{prev:"http://localhost:5173/git/commit", next:"http://localhost:5173/git/"}}
         />
     </section>
   )
