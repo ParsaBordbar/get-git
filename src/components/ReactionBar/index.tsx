76a84beb0142b68bb3d4186428fd285cd7e200ad
parsaBordbar <parsabordbar@parsaBordbars-MacBook-Air.local>
2025-09-14 00:28:54 +0330

    feat(componnets/ReactionBar): responsiveness

--- src/components/ReactionBar/index.tsx
+++ src/components/ReactionBar/index.tsx
@@ -2,38 +2,24 @@ import { ReactionButtonType } from "../../types/types"
 import ReactionButton from "../ReactionButton"
 
 const reactions = [
-    {
-        text: `👍`
-    },
-    {
-      text: '👎'  
-    },
-    {
-        text: `😍`
-    },
-    {
-      text: '😎'  
-    },
-    {
-        text: `💅`
-    },
-    {
-      text: '🤔'  
-    },
+  { text: "👍" },
+  { text: "👎" },
+  { text: "😍" },
+  { text: "😎" },
+  { text: "💅" },
+  { text: "🤔" },
 ]
 
 function ReactionBar() {
   return (
-    <>
-        {/* <hr className="ml-3 mr-20 border-2 rounded-full border-stone-800 flex "/> */}
-        <div className="flex justify-between items-center mx-4 mt-5">
-            {
-                reactions.map((reaction : ReactionButtonType, index:number) => (
-                    <ReactionButton text={reaction.text} key={index}/>                
-                ))
-            }
-        </div>
-    </>
+    <div
+      className="flex flex-wrap justify-center sm:justify-start 
+      gap-2 sm:gap-3 md:gap-4 mx-2 sm:mx-4 mt-4"
+    >
+      {reactions.map((reaction: ReactionButtonType, index: number) => (
+        <ReactionButton text={reaction.text} key={index} />
+      ))}
+    </div>
   )
 }
 
