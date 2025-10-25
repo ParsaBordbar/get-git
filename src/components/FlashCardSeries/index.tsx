cce534babf14ef717ed245a811746f4272b5edeb
ParsaBordbar <parsab71@gmail.com>
2025-10-26 01:31:04 +0330

    feat(FlashCardsSeries): now in liquid glass

--- src/components/FlashCardSeries/index.tsx
+++ src/components/FlashCardSeries/index.tsx
@@ -13,7 +13,7 @@ function FlashCardSeries({
     <section className="grid xl:grid-cols-12 w-fit col-span-full">
       <a
         href={navigation?.prev}
-        className="fixed lg:flex hidden cursor-pointer text-6xl !text-candy-blue left-0 top-1/2 transform -translate-y-1/2 p-4 hover:!text-candy-red delay-150"
+        className="fixed lg:flex hidden cursor-pointer text-5xl !text-white left-0 top-1/2 transform -translate-y-1/2 py-2 px-5 ms-5 glass-gradient glass-text glass-border glass-bg rounded-full glass-btn hover:glass-btn-hover glass-base hover:glass-hover"
         aria-label="Previous"
       >
         {"<"}
@@ -36,7 +36,7 @@ function FlashCardSeries({
       </div>
       <a
         href={navigation?.next}
-        className="fixed lg:flex hidden text-6xl cursor-pointer !text-candy-blue right-0 top-1/2 transform -translate-y-1/2 p-4  hover:!text-candy-red delay-150"
+        className="fixed lg:flex hidden text-5xl cursor-pointer !text-white right-0 top-1/2 transform -translate-y-1/2 delay-150 py-2 px-5 ms-2 glass-gradient glass-text glass-border glass-bg rounded-full glass-btn hover:glass-btn-hover glass-base me-5"
         aria-label="Previous"
       >
         {">"}
